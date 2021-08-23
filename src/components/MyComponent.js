17788f359cc98c6ce3db7210ddfddf32bb3726d4
Eivind Versvik <eivind_versvik@msn.com>
2021-08-23 15:23:13 +0200

    test

---
+++ src/components/MyComponent.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("https://eivindhello.azurewebsites.net/api/stripeapi?")
+        .then((response) => {
+            return response.text();
+        })
+        .then(
+          (result) => {
+            console.log(result);
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, items } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+            <div>gagwa</div>
+        );
+      }
+    }
+  }
+  
+export default MyComponent;
